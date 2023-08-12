ad6ea7b5be28975cbe937ad57dc4f0b1de584b14
YuvalOfek1 <yuvalofek97@gmail.com>
2023-08-12 21:43:49 +0300

    fixed a bug - changed the check song router from  get to post

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/users.controller');
 const validations = require('../validations/index')
 router
 .get('/', userController.getAllUsers)
-.get('/check-song/:id', validations.checkToken, userController.checkSong)
+.post('/check-song/:id', validations.checkToken, userController.checkSong)
 .get('/:id', userController.getUserById)
 .get('/name/:name', userController.getUserByName)
 .get('/email/:email', userController.getUserByEmail)
