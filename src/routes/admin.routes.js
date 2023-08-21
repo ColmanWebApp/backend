2e0add3953a1d475a65b83cfe3d2b5c472a01fe9
YuvalOfek1 <yuvalofek97@gmail.com>
2023-08-21 13:43:14 +0300

    added get all to admin route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,14 +6,14 @@ const songController = require('../controllers/songs.controller');
 const validations = require('../validations/index')
 
 router
-.get('/users',validations.checkToken, validations.adminAuth, userController.updateUser, userController.getAllUsers)
+.get('/users/',validations.checkToken, validations.adminAuth, userController.updateUser, userController.getAllUsers)
 .put('/users/:userId', validations.checkToken, validations.adminAuth, userController.updateUser)
 .delete('/users/:userId', validations.checkToken, validations.adminAuth, userController.deleteUser)
-.get('/', validations.checkToken, validations.adminAuth, songController.getAllSongs)
-.post('/songs', validations.checkToken, validations.adminAuth, songController.createSong)
+.get('/songs/', validations.checkToken, validations.adminAuth, songController.getAllSongs)
+.post('/songs/', validations.checkToken, validations.adminAuth, songController.createSong)
 .put('/songs/:songId', validations.checkToken, validations.adminAuth, songController.updateSong)
 .delete('/songs/:songId', validations.checkToken, validations.adminAuth, songController.deleteSong)
-.get('/', validations.checkToken, validations.adminAuth, orderController.getAllOrders)
+.get('/orders/', validations.checkToken, validations.adminAuth, orderController.getAllOrders)
 .put('/orders/:orderId', validations.checkToken, validations.adminAuth, orderController.updateOrder)
 .delete('/orders/:orderId', validations.checkToken, validations.adminAuth, orderController.deleteOrder)
 
