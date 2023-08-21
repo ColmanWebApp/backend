f389bcb72e5cdf7ec5d9f828a43e459d3af85b1a
YuvalOfek1 <yuvalofek97@gmail.com>
2023-08-21 13:41:20 +0300

    added get all to admin route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,11 +6,14 @@ const songController = require('../controllers/songs.controller');
 const validations = require('../validations/index')
 
 router
+.get('/',validations.checkToken, validations.adminAuth, userController.updateUser, userController.getAllUsers)
 .put('/users/:userId', validations.checkToken, validations.adminAuth, userController.updateUser)
 .delete('/users/:userId', validations.checkToken, validations.adminAuth, userController.deleteUser)
+.get('/', validations.checkToken, validations.adminAuth, songController.getAllSongs)
 .post('/songs', validations.checkToken, validations.adminAuth, songController.createSong)
 .put('/songs/:songId', validations.checkToken, validations.adminAuth, songController.updateSong)
 .delete('/songs/:songId', validations.checkToken, validations.adminAuth, songController.deleteSong)
+.get('/', validations.checkToken, validations.adminAuth, orderController.getAllOrders)
 .put('/orders/:orderId', validations.checkToken, validations.adminAuth, orderController.updateOrder)
 .delete('/orders/:orderId', validations.checkToken, validations.adminAuth, orderController.deleteOrder)
 
