64c6d5dddcf4e7677c7e504417c6f749d6dc5019
YuvalOfek1 <yuvalofek97@gmail.com>
2023-08-24 16:42:21 +0300

    added socket io

--- src/utils/sockets.js
+++ src/utils/sockets.js
@@ -2,18 +2,20 @@ const xss = require("xss");
 
 const handleClient = (io) => {
   io.on("connection", (socket) => {
-    console.log("New WS Connection...");
+    console.log("New WebSocket Connection...");
 
     // Listen for incoming chat messages
     socket.on("message", (message) => {
       console.log("message: ", message);
 
       // Sanitize the message before broadcasting
-      const sanitizedMessage = xss.escapeHtml(message);
+      const sanitizedMessage = xss.escapeHtml("hello from server!");
 
       // Broadcast the sanitized message to all connected clients
       io.emit("message", sanitizedMessage);
     });
+
+    
 
     socket.on("disconnect", () => {
       io.emit("message", "A user has left the chat");
